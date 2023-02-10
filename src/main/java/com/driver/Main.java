c8b462814f755fd017f2cbf4e163ed6f87d1c0a2
ShravanJadhav <shravanjadhav844@gmail.com>
2023-02-10 12:07:53 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[]args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Shravan");
+        obj.getName();
+    }
   
 }
